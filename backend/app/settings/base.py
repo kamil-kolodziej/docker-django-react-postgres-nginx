236d7e8bc81ed0535dec8346378145dac6424f53
Kamil Kołodziej <kamil.kolodziej01@gmail.com>
2021-02-27 14:23:59 +0100

    Django static files

--- backend/app/settings/base.py
+++ backend/app/settings/base.py
@@ -120,4 +120,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/api/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'api/static')
