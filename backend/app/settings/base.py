a11aa43035a2e9a76178e726e2d7cdf1bf17d981
Kamil Kołodziej <kamil.kolodziej01@gmail.com>
2021-02-27 15:10:53 +0100

    Production frontend app

--- backend/app/settings/base.py
+++ backend/app/settings/base.py
@@ -14,7 +14,7 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
+BASE_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
